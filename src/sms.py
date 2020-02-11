97ee6afe33ae11bc9a506ab4da870b90de2e9beb
kevin <fsd83811@gmail.com>
2020-02-11 21:30:35 +0800

    LR

--- src/sms.py
+++ src/sms.py
@@ -5,9 +5,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer as SI
 from sklearn.linear_model import LinearRegression as LR 
 from sklearn.preprocessing import LabelEncoder as LE, OneHotEncoder as OHE
+from sklearn import preprocessing
 
 # read
-input_data = pd.read_csv('data analytics 154.csv',encoding="utf-8")
+input_data = pd.read_csv('data\\data analytics 154.csv',encoding="utf-8")
 #print(input_data)
 
 #提取CSV檔 標籤
@@ -46,11 +47,25 @@ for i in [0,5,6,7]:
 ohe_enc = OHE(categorical_features=[0])
 #data = ohe_enc.fit_transform(data).toarray()
 #data = data.astype(int)
+
+for i in range(8):
+    data[:,i] = preprocessing.scale(data[:,i])
 print(pd.DataFrame(data))
 
+# str get int
+for i in range(len(label)):
+    if(label[i].find('Top ') != -1):
+        label[i] = label[i].replace('Top ', '')
+    elif(label[i].find('outside top ') != -1):
+        label[i] = label[i].replace('outside top ', '50')
+    label[i] = int(label[i])
+#print('str get int', label)
+'''
+# str to value
 label_enc = LE()
 label = label_enc.fit_transform(label)
 print(pd.DataFrame(label))
+'''
 
 import numpy as np
 import pandas as pd
@@ -63,12 +78,13 @@ from sklearn.datasets import load_boston
 from subroutine.CMatrix import cm_plot     #副程式
 from subroutine.ROC_AUC import acu_curve   #副程式
 from sklearn import preprocessing
+'''
 x = data   #提取權重較重的資料
 y = label
 x = preprocessing.scale(x)
 #print(pd.DataFrame(x))
 
-'''
+
 y_median = np.median(pd.DataFrame(set(y)))      #計算y資料的中位數
 for i in range(len(y)):                         #以中位數把y資料切成0,1
     if y[i] > y_median:
@@ -77,7 +93,7 @@ for i in range(len(y)):                         #以中位數把y資料切成0,1
         y[i] = 0
 #print(x)        
 #print(y)
-'''
+
 knn = KNeighborsClassifier(n_neighbors=4)      #用KNN分類法,尋找4個鄰居
 cv = ShuffleSplit(n_splits=10, test_size=0.4)   # K為10，將資料分成train,test
 scores_clf_svc_cv = cross_val_score(knn, x, y, cv=cv, scoring='accuracy')
@@ -94,7 +110,7 @@ y_pred_proba = knn.predict_proba(X_test)[:,1]   #訓練出來的資料
 print("accuracy score:", accuracy_score(y_test, y_predict))     #計算模型準確度
 #cm_plot(y_test, y_predict)      #計算混淆矩陣
 print('----------------------------------')
-'''
+
 acu_curve(y_test, y_pred_proba) #KNN計算真正率 & 假正率
 print('----------------------------------')
 
@@ -102,6 +118,7 @@ svm = svm.SVC(kernel='linear', probability=True)
 y_score = svm.fit(X_train, y_train).decision_function(X_test)
 acu_curve(y_test, y_score)      #SVM計算真正率 & 假正率
 print('----------------------------------')
+'''
 '''
 #尋找鄰居最好的K值
 #k_range = range(1,50)
@@ -114,12 +131,30 @@ print('----------------------------------')
 #plt.xlabel('Value of K for KNN')
 #plt.ylabel('Cross-Validated Accuracy')
 #plt.show()
+'''
 
-y_predict = knn.predict(data)
-print(pd.DataFrame(label_enc.inverse_transform(y_predict)))
-print(pd.DataFrame(label))
-print(pd.DataFrame(label_enc.inverse_transform(label)))
+# 訓練並擬合後測試
+regs = LR()
+regs.fit(data, label)
 
+# 測試6月份資料
+result = regs.predict(data)
+
+# 驗證100比
+#y_predict = knn.predict(data)
+#print(pd.DataFrame(label_enc.inverse_transform(result)))
+#print(pd.DataFrame(label_enc.inverse_transform(label)))
+#print(pd.DataFrame(result))
+#print(pd.DataFrame(label))
+labelSqu = label.reshape(-1, 1)
+resultSqu = result.reshape(-1, 1)
+
+for i in range(len(label)):
+    print(label[i], result[i])
+
+from sklearn.metrics import mean_squared_error, r2_score
+print("score:", r2_score(label, result.round()))    #計算模型準確度
+'''
 while 1:
     # 使用者輸入資料
     ctData = np.full((1,8), None)
@@ -130,8 +165,10 @@ while 1:
     for i in [0,5,6,7]:
         ctData[:,i] = data_enc[i].transform(ctData[:,i]) # str to int
     #print(ctData)
-
+'''
+'''
     # 即時預測
     y_predict = knn.predict(ctData) # int to str
-    print(label_enc.inverse_transform(y_predict))
-
+    #print(label_enc.inverse_transform(y_predict))
+    print(y_predict)
+'''
